65d394ffd24a33a355322efb099c7d37de899174
matoval <mat24c@gmail.com>
2020-02-24 10:36:14 -0800

    removed some console logs

--- src/App.svelte
+++ src/App.svelte
@@ -2,28 +2,6 @@
 import Topbar from "./Topbar.svelte";
 import Bottombar from "./Bottombar.svelte";
 
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
-const spell = new SpeechRecognition();
-
-spell.addEventListener('result', e =>{
-	const transcript = Array.from(e.results)
-	.map(result => result[0])
-	.map(result => result.transcript)
-	.join('');
-	console.log(transcript);
-	if (transcript === "Knox") {
-		document.body.style = "background-color: black;"
-	}
-	else if (transcript === "lumos") {
-		document.body.style = "background-color: white;"
-	}
-})
-
-spell.addEventListener('end', spell.start)
-
-spell.start();
-
 </script>
 
 <main id="whole-page">
